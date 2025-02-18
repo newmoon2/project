4d0ce119e04456174aa91ea293768d086db1c55d
object <object@3softplus.com>
2025-02-18 11:02:24 +0900

    install

--- ThinkRAG_mod/start.sh
+++ ThinkRAG_mod/start.sh
@@ -1,9 +1,7 @@
 !/bin/bash
 
-cd /home/
-git clone https://github.com/newmoon2/project.git
-echo "git clone completed.."
 cd /home/project/ThinkRAG_mod
+
 pip3 install -r requirements.txt
 echo "dependency install completed.."
 streamlit run app.py
